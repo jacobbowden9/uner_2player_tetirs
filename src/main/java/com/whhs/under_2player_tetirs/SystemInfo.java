46b2bcfbe50b9d0aeb3bbdc915e4f71d11559d4a
WDAGUtilityAccount <WDAGUtilityAccount@cc3f4d56-77c0-444e-8605-10ff10869510.mshome.net>
2023-12-14 11:02:14 -0600

    first commit

---
+++ src/main/java/com/whhs/under_2player_tetirs/SystemInfo.java
@@ -0,0 +1,13 @@
+package com.whhs.under_2player_tetirs;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module com.whhs.under_2player_tetirs {
+    requires javafx.controls;
+    exports com.whhs.under_2player_tetirs;
+}
